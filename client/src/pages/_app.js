eeb4a8809c7d0737a75c871fde00af23876f8660
rainz728 <rainzhu0728@gmail.com>
2026-08-22 17:33:40 +1000

    Load game and map styles

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,4 +1,9 @@
+import "leaflet/dist/leaflet.css";
+
 import "../styles/globals.css";
+import "../components/GameScreen/GameScreen.css";
+import "../components/HomeScreen/HomeScreen.css";
+import "../components/Lobby/Lobby.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
